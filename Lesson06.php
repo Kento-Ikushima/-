33de1a76656e794a63c91d5c78e1d110315a080f
Kento-Ikushima <114498754+Kento-Ikushima@users.noreply.github.com>
2022-10-02 12:40:27 +0900

    initial commit.
    練習です。

---
+++ Lesson06.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>aiuo</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<heaader>
+    <h1><a href="#"></a><img src="logo.png" alt="ロゴ"></h1>
+    <p>03-0000-0000</p>
+</header>
+
+<nav>
+    <ul>
+        <li><a href="#">ホーム</a></li>
+        <li><a href="#">サービス</a></li>
+        <li><a href="#">製品FAQ</a></li>
+        <li><a href="#">お問い合わせ</a></li>
+    </ul>
+</nav>
+
+<h2>テンプレートのご利用前に必ずお読み下さい</h2>
+<h3>利用規約のご案内</h3>
+<hl>
+<p>このテンプレートは、Template Partyにて無料配布している『企業・ビジネスサイト向け 無料ホームページテンプレート tp_biz56』です。必ずダウンロード先のサイトの利用規約をご一読の上でご利用下さい。</p>
+<p>HP最下部の著作表示『Web Design:Template-Party』は無断で削除しないで下さい。</p>
+<p>わざと見えなく加工する事も禁止です。</p>
+<p>下部の著作を外したい場合は</p>
+<p>Template-Partyのライセンス契約を行う事でHP下部の著作を外す事ができます。</p>
+</body>
+
+
+
+
+
+
+
+
+
+
